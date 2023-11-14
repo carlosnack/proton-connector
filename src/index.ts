1c4fc2aa57d2e6feeba1c7aeb7a40c77af6a8bc7
Carlos Nackamura <c.nackamura@unesp.br>
2023-11-14 09:19:47 -0300

    fix: fixing app listen index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,11 @@ const app = express()
 const port = 3000
 app.use('/api', router) // Use o controlador para a rota '/api/estoque'
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
-})
 sequelize
   .sync()
   .then(() => {
     console.log('Tabelas sincronizadas')
-    app.listen(3000, () => {
+    app.listen(port, () => {
       console.log('Servidor rodando na porta 3000')
     })
   })
