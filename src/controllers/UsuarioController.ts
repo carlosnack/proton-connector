651e677329710d1fe6df141a55c335f8166056bf
Vinicius Prado <vinicavagna@gmail.com>
2023-11-28 18:13:47 -0300

    change tsconfig and docker config

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -3,25 +3,31 @@ import { validationResult } from "express-validator";
 import UsuarioService from "../services/UsuarioService";
 
 export default class UsuarioController {
-    static async criarRegistroDeUsuario(
-        req: Request, res: Response
-    ) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
-        const { userID, name, password, email, active } = req.query;
-        const user = await UsuarioService.criarRegistroDeUsuario(userID as any as number, name as string, password as string, email as string, active as any as boolean);
-        res.status(200).json({ user });
+  static async criarRegistroDeUsuario(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    static async signIn(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
-        const {password, email} = req.query;
-        const userLogin = await UsuarioService.signIn(password as string, email as string);
-        res.status(200).json( userLogin );
+    const { userID, name, password, email, active } = req.query;
+    const user = await UsuarioService.criarRegistroDeUsuario(
+      userID as any as number,
+      name as string,
+      password as string,
+      email as string,
+      active as any as boolean
+    );
+    return res.status(200).json({ user });
+  }
+  static async signIn(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-
+    const { password, email } = req.query;
+    const userLogin = await UsuarioService.signIn(
+      password as string,
+      email as string
+    );
+    return res.status(200).json(userLogin);
+  }
 }
